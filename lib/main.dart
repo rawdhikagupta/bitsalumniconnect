89e9f4b368e82f8ba9540bde05bcd76c6f2c4543
Shrestha29 <75108462+Shrestha29@users.noreply.github.com>
2021-08-21 23:07:15 +0530

    Add files via upload
    Progress bar still needs to be worked upon.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,109 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
-import 'page 6.dart';
 
-void main() {
-  runApp(MyApp());
+class _Pagethree extends StatefulWidget {
+  const _Pagethree({Key? key}) : super(key: key);
+
+  @override
+  _PagethreeState createState() => _PagethreeState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _PagethreeState extends State<_Pagethree> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Page_six(),
-    );
-  }
-}
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              alignment: Alignment.topRight,
+              margin: EdgeInsets.all(10),
+              child: ButtonTheme(
+                minWidth: 82,
+                height: 40,
+                child: OutlineButton(
+                  color: Colors.black,
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Help ?',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  borderSide: BorderSide(
+                    color: Color(0xFFA0A0A0),
+                    style: BorderStyle.solid,
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Text('AlmaConnect \nBITS',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            Card(elevation: 6,
+            margin: EdgeInsets.all(20),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 75,
+                      width: 75,
+                      child: Image.asset('images\face.png'),
+                    ),
+                    Text('I am a Mentee', textAlign: TextAlign.center, style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                    )),
+                    Container(
+                      height: 44,
+                      width: 52,
+                      child: Image.asset('images\arrow.png'),
+                      ), ],
+                ),
+            ),
+            ),
+            Card(elevation: 6,
+            margin: EdgeInsets.all(20),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 75,
+                      width: 75,
+                      child: Image.asset('images\face.png'),
+                    ),
+                    Text('I am a Mentor', textAlign: TextAlign.center, style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 20,
+                    )),
+                    Container(
+                      height: 44,
+                      width: 52,
+                      child: Image.asset('images\arrow.png'),
+                      ), ],
+                ),
+            ),
+            ),
+          ],),
+          ),),
+          );
+          }
+          }
+
