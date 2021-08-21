572b4defe1ff65fb62decd1255769a750d3d8c33
rawdhikagupta <rawdhikagupta@gmail.com>
2021-08-21 13:58:11 +0530

    fixed overflow issue

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,50 +11,52 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(44, 70, 0, 0),
-                  child: Text(
-                    'Welcome to',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 35,
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(40, 0, 0, 0),
-                  child: Text('AlmaConnect \nBITSP',
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(44, 70, 0, 0),
+                    child: Text(
+                      'Welcome to',
                       style: TextStyle(
                         fontFamily: 'Roboto',
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ),
-                Image.asset('images/graduation.png'),
-                Center(
-                  child: RaisedButton(
-                    onPressed: () {
-                      print('button was pressed');
-                    },
-                    color: Color(0xFF4B2E83),
-                    shape:
-                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(40, 16, 40, 16),
-                      child: Text('GET STARTED',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          )),
+                        fontSize: 35,
+                      ),
+                      textAlign: TextAlign.left,
                     ),
                   ),
-                )
-              ],
+                  Container(
+                    margin: EdgeInsets.fromLTRB(40, 0, 0, 0),
+                    child: Text('AlmaConnect \nBITSP',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                  Image.asset('images/graduation.png'),
+                  Center(
+                    child: RaisedButton(
+                      onPressed: () {
+                        print('button was pressed');
+                      },
+                      color: Color(0xFF4B2E83),
+                      shape:
+                      RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(40, 16, 40, 16),
+                        child: Text('GET STARTED',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            )),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )));
   }
 }
--- lib/page 6.dart
+++ lib/page 6.dart
@@ -12,98 +12,99 @@ class _Page_sixState extends State<Page_six> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(children: [
-          Container(
-            alignment: Alignment.topRight,
-            margin: EdgeInsets.all(10),
-            child: ButtonTheme(
-              minWidth: 82,
-              height: 40,
-              child: OutlineButton(
-                color: Colors.black,
-                onPressed: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Help ?',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              alignment: Alignment.topRight,
+              margin: EdgeInsets.all(10),
+              child: ButtonTheme(
+                minWidth: 82,
+                height: 40,
+                child: OutlineButton(
+                  color: Colors.black,
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Help ?',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
                     ),
                   ),
-                ),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50)),
-                borderSide: BorderSide(
-                  color: Color(0xFFA0A0A0),
-                  style: BorderStyle.solid,
-                  width: 2,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  borderSide: BorderSide(
+                    color: Color(0xFFA0A0A0),
+                    style: BorderStyle.solid,
+                    width: 2,
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            child: Text('AlmaConnect \nBITS',
+            Container(
+              child: Text('AlmaConnect \nBITS',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+              alignment: Alignment(-0.2, 0),
+              child: Text(
+                'Great You are paired! \nSend a Quick\nMessage',
                 style: TextStyle(
                   fontFamily: 'Roboto',
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
-            alignment: Alignment(-0.2, 0),
-            child: Text(
-              'Great You are paired! \nSend a Quick\nMessage',
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 24,
+                  fontSize: 24,
+                ),
               ),
             ),
-          ),
-          LeftChatbox('Hello!',36),
-          Container(
-            alignment: Alignment.topRight,
-            padding: EdgeInsets.only(right:39),
-            child: Stack(
-                children: [
-              Image.asset('images/icon2.png'),
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Text('Excited to\nmeet you!',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 20,
-                    )),
-              ),
-            ]),
-          ),
-          LeftChatbox('My name\nis Radhika', 20),
-          Container(
-            padding: EdgeInsets.only(top:20),
-            child: ButtonTheme(
-              minWidth: 280,
-              height: 61,
-              child: RaisedButton(
-                onPressed: () {
-                  print('button was pressed');
-                },
-                color: Color(0xFF4B2E83),
-                shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-                  child: Text('Home',
+            LeftChatbox('Hello!', 36),
+            Container(
+              alignment: Alignment.topRight,
+              padding: EdgeInsets.only(right: 39),
+              child: Stack(children: [
+                Image.asset('images/icon2.png'),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Text('Excited to\nmeet you!',
                       style: TextStyle(
-                        color: Colors.white,
+                        fontFamily: 'Roboto',
                         fontSize: 20,
                       )),
                 ),
-              ),
+              ]),
             ),
-          )
-        ]),
+            LeftChatbox('My name\nis "name"', 20),
+            Container(
+              padding: EdgeInsets.only(top: 20),
+              child: ButtonTheme(
+                minWidth: 280,
+                height: 61,
+                child: RaisedButton(
+                  onPressed: () {
+                    print('button was pressed');
+                  },
+                  color: Color(0xFF4B2E83),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    child: Text('Home',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        )),
+                  ),
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
@@ -111,23 +112,25 @@ class _Page_sixState extends State<Page_six> {
 
 class LeftChatbox extends StatelessWidget {
   String text;
-  double textsize=0;
-   LeftChatbox(this.text, this.textsize);
+  double textsize = 0;
+  LeftChatbox(this.text, this.textsize);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.topLeft,
-      padding: EdgeInsets.only(left:39),
-      child: Stack(
-           children:[Image.asset('images/icon1.png'), Padding(
-             padding: const EdgeInsets.all(30.0),
-             child: Text('$text',
-                 style: TextStyle(
-                   fontFamily: 'Roboto',
-                   fontSize:textsize,
-                 )),
-           ),]),
+      padding: EdgeInsets.only(left: 39),
+      child: Stack(children: [
+        Image.asset('images/icon1.png'),
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Text('$text',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: textsize,
+              )),
+        ),
+      ]),
     );
   }
 }
